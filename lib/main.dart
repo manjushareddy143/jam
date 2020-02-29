e8af5600541a6556f6aa95fe55e6897b0c68e9e3
ak <ak@savitriya.com>
2020-02-29 10:13:19 +0530

    view api

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(new MaterialApp(
@@ -17,31 +17,65 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
 
-  String data;
-
-//Api call n responce method
+  List data;
 
   Future<String> getData() async {
-    http.Response  response = await http.get(
+    var response = await http.get(
         Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
         headers: {
           "Accept": "application/json"
         }
     );
-    data=json.decode(response.body);
-    print(response.body);
 
-    return "Success!";
+    this.setState(() {
+      data = json.decode(response.body);
+    });
+
+    print(data[1]["title"]);
+
+    return data.map<Post>((json) => Post.fromJson(json)).toList();
   }
+
+  class Post{
+    final int userId;
+    final int id;
+    final String title;
+
+    Post(
+  {this.userId, this.id, this.title});
+
+    factory Post.fromJson(Map<String, dynamic> json){
+      return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      );
+  }
+
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    this.getData();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Center(
-        child: new RaisedButton(
-          child: new Text("Get API requested data"),
-          onPressed: getData,
-        ),
+      appBar: new AppBar(
+        title: new Text("List"),
+      ),
+      body: new ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+            child: new Text(data[index]["title"]),
+
+          );
+        },
       ),
     );
   }
