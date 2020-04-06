76211cd3b3e7e14c2f35a5e0c48855b398c60a80
aparna1463 <ak@savitriya.com>
2020-04-06 12:09:48 +0530

    Update home_widget.dart

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -61,7 +61,7 @@ class _HomeState extends State<Home> {
         unselectedItemColor: Colors.grey,
         backgroundColor: Colors.white,
         iconSize: 30,
-        
+
         type: BottomNavigationBarType.fixed,
 
         items: [ BottomNavigationBarItem(
