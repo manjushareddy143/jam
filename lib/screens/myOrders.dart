579d123092a1cfb7b654bc946f673d1c7d49a324
aparna1463 <ak@savitriya.com>
2020-05-01 19:15:26 +0530

    myOrder Overflow Fixed

--- lib/screens/myOrders.dart
+++ lib/screens/myOrders.dart
@@ -38,19 +38,20 @@ class _OrderUIPageState extends State<OrderUIPage> {
                   child: Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     // alignment: WrapAlignment.spaceEvenly,
                     children: <Widget>[
-                      Container(margin: EdgeInsets.all(20),
+                      Container(margin: EdgeInsets.all(18),
                         alignment: Alignment.center,padding: EdgeInsets.only(top: 10),
                         width: 100,
                         height: 100,
                         decoration: new BoxDecoration(shape: BoxShape.circle, image: new DecorationImage(fit: BoxFit.fill, image: new AssetImage("assets/images/vicky.jpg")) ),
                       ),
 
-                      Container(width: 240,margin: EdgeInsets.all(10),
+                      Container(width: 240,margin: EdgeInsets.fromLTRB(10,10,20,10),
                         height: 120,
+                        
                         child:
                         Column(crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
+                          children: [
                             new Text(
                               "Afrar Sheikh",
                               textAlign: TextAlign.left,
@@ -61,66 +62,68 @@ class _OrderUIPageState extends State<OrderUIPage> {
                               "Plumbing",
                               textAlign: TextAlign.left,
                               overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15.0,),
+                              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16.0,),
                             ),
                             new Text(
                               "Order#:990245",
                               textAlign: TextAlign.left,
                               overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15.0,),
+                              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16.0,),
                             ),
-                            Wrap(
-                                children:<Widget> [
-                                  Row(
-                                      children:<Widget>[
-                                        new Text(
-                                          "01-April-2020, 1:00 PM",
-                                          textAlign: TextAlign.left,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 13.0, ),
-                                        ),
-                                        SizedBox(width: 10,),
-                                        new SmoothStarRating(
-                                            allowHalfRating: false,
-
-                                            starCount: 5,
-                                            rating: 3.0,
-                                            size: 15.0,
-                                            filledIconData: Icons.star,
-                                            halfFilledIconData: Icons.star,
-                                            color: Colors.amber,
-                                            borderColor: Colors.amber,
-                                            //unfilledStar: Icon(Icons., color: Colors.grey),
-                                            spacing:0.0
-                                        )
-                                      ]
+                            Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children:<Widget>[
+                                  Flexible(
+                                    flex:2,
+                                    child: new Text(
+                                      "01-April-2020, 1:00 PM",
+                                      textAlign: TextAlign.left,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(fontWeight: FontWeight.w400, fontSize: 13.0, ),
+                                    ),
                                   ),
+                                 // SizedBox(width: 10,),
+
+
+                                  Flexible(
+                                    flex: 1,
+                                    child: SmoothStarRating(
+                                        allowHalfRating: false,
+
+                                        starCount: 5,
+                                        rating: 3.0,
+                                        size: 15.0,
+                                        filledIconData: Icons.star,
+                                        halfFilledIconData: Icons.star,
+                                        color: Colors.amber,
+                                        borderColor: Colors.amber,
+                                        //unfilledStar: Icon(Icons., color: Colors.grey),
+                                        spacing:0.0
+                                    ),
+                                  )
                                 ]
                             ),
                             SizedBox(height: 20,),
                             Container(
                               child:
                               Row(//crossAxisAlignment: WrapCrossAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
-                                    new Text(
-                                      "Order Completed",
-                                      textAlign: TextAlign.center,
-                                      overflow: TextOverflow.ellipsis,
+                                    Flexible(
+                                      child: new Text(
+                                        "Order Completed",
+                                        textAlign: TextAlign.center,
+                                        overflow: TextOverflow.ellipsis,
 
-                                      style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12.0, color: Colors.teal, ),
+                                        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 13.0, color: Colors.teal, ),
 
+                                      ),
                                     ),
 
-                                    Icon(Icons.check_box, size: 12,color: Colors.teal,)]
+                                    Flexible( child: Icon(Icons.check_box, size: 12,color: Colors.teal,))]
                               ),
 
 
-                            )
-
-
-
-
+                            ),
                           ],
                         ),
                       ),
