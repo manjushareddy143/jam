ff97e229948dfe803bef70aa4ba01441a1c3a938
manjushareddy143 <manjushamac@gmail.com>
2021-09-13 21:21:08 +0300

    new changes

--- lib/screens/initial_profile.dart
+++ lib/screens/initial_profile.dart
@@ -8,7 +8,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter/widgets.dart';
+ 
 import 'package:geopoint_location/geopoint_location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:jam/models/service.dart';
